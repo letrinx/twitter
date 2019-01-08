e227bf6598beae1ef008fa439f574fe4445689f8
letrinx <gregzaba1@gmail.com>
2019-01-08 22:57:55 +0100

    Add model Twitt

--- twitts/models.py
+++ twitts/models.py
@@ -1,3 +1,12 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Twitt(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    content = models.CharField(max_length=150)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.content)
